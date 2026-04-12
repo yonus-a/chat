9447c4e29f230d033e5ed1c5ed43672ee350f3d6
Amir Safari <a.safari1380@gmail.com>
2026-04-12 08:12:24 +0330

    making the translations

---
+++ app/pages/auth.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class=" w-full flex flex-col items-center gap-y-16 h-full">
+        <div></div>
+    </div>
+</template>
+
+<script lang="ts">
+import LocaleSwitch from '~/components/general/LocaleSwitch.vue';
+import ThemeSwitch from '~/components/general/ThemeSwitch.vue';
+
+definePageMeta({
+    layout: 'dashboard'
+})
+
+export default defineComponent({
+    name: 'AuthBasePage',
+    components: {
+        ThemeSwitch,
+        LocaleSwitch,
+    }
+})
+</script>
--- app/pages/auth/index.vue
+++ app/pages/auth/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>Hello</div>
+    <div>سلام</div>
 </template>
 <script lang="ts">
 
