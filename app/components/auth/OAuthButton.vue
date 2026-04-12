2589686c18ee55955b0687302d3beb7e65bf54da
Amir Safari <a.safari1380@gmail.com>
2026-04-12 12:43:35 +0330

    added oauth button

---
+++ app/components/auth/OAuthButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <div
+        class=" h-11 transition-all  duration-200 ease-in-out hover:bg-surface-variant-3 w-full cursor-pointer select-none text-label-md text-on-surface bg-surface-variant-2 rounded-xl flex items-center justify-between p-3">
+        <div class=" w-5 h-5">
+            <BImage class=" min-w-full min-h-full max-w-full max-h-full w-full h-full" :src="logo" />
+
+        </div>
+        <div>{{ }}</div>
+        <div></div>
+    </div>
+</template>
+<script lang="ts" setup>
+// t('auth.login.googleLogin')
+defineProps({
+    
+})
+
+</script>
--- app/pages/auth/index.vue
+++ app/pages/auth/index.vue
@@ -13,7 +13,16 @@
                     }}</div>
                     <div class=" flex-1 border border-outline"></div>
                 </div>
-
+               
+                <div
+                    class=" h-11 transition-all  duration-200 ease-in-out hover:bg-surface-variant-3 w-full cursor-pointer select-none text-label-md text-on-surface bg-surface-variant-2 rounded-xl flex items-center justify-between p-3">
+                    <div class=" w-5 h-5">
+                        <BImage class=" min-w-full min-h-full max-w-full max-h-full w-full h-full"
+                            :src="dolateManLogo" />
+                    </div>
+                    <div>{{ t('auth.login.dolatLogin') }}</div>
+                    <div></div>
+                </div>
             </div>
         </div>
     </div>
