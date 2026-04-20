9826c4252903b048998ba68338fc8575f47916f5
Amir Safari <a.safari1380@gmail.com>
2026-04-20 12:31:19 +0330

    fixed style issues

--- app/components/layout/dashboard/MobileNavigation.vue
+++ app/components/layout/dashboard/MobileNavigation.vue
@@ -1,27 +1,31 @@
 <template>
-    <Teleport to="body">
-        <div
-            class=" fixed h-14 shadow-medium gap-x-2 bottom-0 left-0 w-dvw bg-surface z-50 lg:hidden py-0.75 px-3 flex items-center justify-between">
-            <div @click="openRoute(route.route)" v-for="route in routes" :key="route.key" class="basis-1/5 h-full ">
-                <div class="  h-full w-full rounded-xl flex-col gap-y-0.5 flex justify-center items-center transition-all duration-200 ease-in-out cursor-pointer select-none"
-                    :class="[isRouteActive(route.route) ? ' bg-primary/10' : 'bg-primary/0']">
-                    <BIcon :icon="route.icon" class=" w-5 h-5 transition-all duration-200 ease-in-out"
-                        :class="[isRouteActive(route.route) ? ' fill-primary' : ' fill-on-surface/50']" />
-                    <div class=" select-none text-on-surface text-[10px]">{{ route.label }}</div>
+    <ClientOnly>
+        <Teleport to="body">
+            <div
+                class="fixed bottom-0 inset-x-0 h-14 shadow-medium gap-x-2 w-full bg-surface z-50 lg:hidden py-1 px-3 flex items-center justify-between">
+                <div @click="openRoute(route.route)" v-for="route in routes" :key="route.key" class="basis-1/5 h-full">
+                    <div class="h-full w-full rounded-xl flex-col gap-y-0.5 flex justify-center items-center transition-all duration-200 ease-in-out cursor-pointer select-none"
+                        :class="[isRouteActive(route.route) ? 'bg-primary/10' : 'bg-primary/0']">
+                        <BIcon :icon="route.icon" class="w-5 h-5 transition-all duration-200 ease-in-out"
+                            :class="[isRouteActive(route.route) ? 'fill-primary' : 'fill-on-surface/50']" />
+                        <div class="select-none text-on-surface text-[10px]">{{ route.label }}</div>
+                    </div>
                 </div>
-            </div>
-            <div @click="openRoute('/dashboard/profile')"
-                class=" h-full flex-col gap-y-0.5 basis-1/5 aspect-square flex items-center justify-center">
-                <div class=" w-5 h-5 rounded-full overflow-hidden">
-                    <BImage :src="profileImage" class=" w-full h-full min-w-full min-h-full max-w-full max-h-full" />
+
+                <div @click="openRoute(localePath('/dashboard/profile'))"
+                    class="h-full flex-col gap-y-0.5 basis-1/5 aspect-square flex items-center justify-center cursor-pointer">
+                    <div class="w-5 h-5 rounded-full overflow-hidden border border-outline/20">
+                        <BImage :src="profileImage" class="w-full h-full object-cover" />
+                    </div>
+                    <div class="select-none text-on-surface text-[10px]">{{ t('sidebar.profile') }}</div>
                 </div>
-                <div class=" select-none text-on-surface text-[10px]">{{ t('sidebar.profile') }}</div>
+
+                <NuxtLinkLocale to="/dashboard/services" class="flex items-center justify-center">
+                    <BButton icon="PhPlus" size="sm" />
+                </NuxtLinkLocale>
             </div>
-            <NuxtLinkLocale to="/dashboard/services">
-                <BButton icon="PhPlus" />
-            </NuxtLinkLocale>
-        </div>
-    </Teleport>
+        </Teleport>
+    </ClientOnly>
 </template>
 <script lang="ts">
 import { defineComponent, computed } from 'vue';
@@ -76,6 +80,7 @@ export default defineComponent({
         return {
             isRouteActive,
             routes,
+            localePath,
             openRoute,
             profileImage,
             t,
